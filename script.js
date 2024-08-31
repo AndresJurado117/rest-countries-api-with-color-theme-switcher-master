975b52e89ac0878c7c952de969e4b79daaa20909
Andres Jurado <89615507+AndresJurado117@users.noreply.github.com>
2024-08-30 22:06:05 -0500

    Cleaned JS

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     const countriesDiv = document.getElementById('countries');
 
-    function countries(region='') {
+    function countries(country) {
+        var container = document.createElement('div');
+        container.textContent = country.name;
+        var countryImage = document.createElement('img');
+        countryImage.src = country.flags.png;
+        container.appendChild(countryImage);
+        var countryPopulation = document.createElement('p');
+        countryPopulation.innerText = `Population: ${country.population}`;
+        container.appendChild(countryPopulation);
+        var countryRegion = document.createElement('p');
+        countryRegion.innerText = `Region: ${country.region}`;
+        container.appendChild(countryRegion);
+        var countryCapital = document.createElement('p');
+        countryCapital.innerText = `Capital: ${country.capital}`;
+        container.appendChild(countryCapital);
+        countriesDiv.append(container);
+    }
+
+    function filterRegion(region='') {
         countriesDiv.innerHTML = '';
         if (region == '') {
             fetch('./data.json')
             .then((response) => response.json())
             .then((data) => data.forEach(country => {
-                var container = document.createElement('div');
-                container.textContent = country.name;
-                var countryImage = document.createElement('img');
-                countryImage.src = country.flags.png;
-                container.appendChild(countryImage);
-                var countryPopulation = document.createElement('p');
-                countryPopulation.innerText = `Population: ${country.population}`;
-                container.appendChild(countryPopulation);
-                var countryRegion = document.createElement('p');
-                countryRegion.innerText = `Region: ${country.region}`;
-                container.appendChild(countryRegion);
-                var countryCapital = document.createElement('p');
-                countryCapital.innerText = `Capital: ${country.capital}`;
-                container.appendChild(countryCapital);
-                countriesDiv.append(container);
+                countries(country);
             }));
         }
         else {
@@ -29,32 +33,17 @@ document.addEventListener('DOMContentLoaded', () => {
             .then((response) => response.json())
             .then((data) => data.forEach(country => {
                 if(country.region == region) {
-                    console.log(country.name);
-                    var container = document.createElement('div');
-                    container.textContent = country.name;
-                    var countryImage = document.createElement('img');
-                    countryImage.src = country.flags.png;
-                    container.appendChild(countryImage);
-                    var countryPopulation = document.createElement('p');
-                    countryPopulation.innerText = `Population: ${country.population}`;
-                    container.appendChild(countryPopulation);
-                    var countryRegion = document.createElement('p');
-                    countryRegion.innerText = `Region: ${country.region}`;
-                    container.appendChild(countryRegion);
-                    var countryCapital = document.createElement('p');
-                    countryCapital.innerText = `Capital: ${country.capital}`;
-                    container.appendChild(countryCapital);
-                    countriesDiv.append(container);
+                    countries(country);
                 }
             }));
         }
     }
 
-    countries();
+    filterRegion();
 
     const regionSelected = document.getElementById('region');
 
     regionSelected.onchange = function() {
-        countries(this.value);
+        filterRegion(this.value);
     }
 })
