a241ad9fc308a6cd9ac36a385f24f4df8bcd50c8
Andres Jurado <89615507+AndresJurado117@users.noreply.github.com>
2024-09-03 23:17:20 -0500

    Almost finished CSS and JS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const countriesDiv = document.getElementById('countries');
-    const countrySearch = document.getElementById('country_search');
+    const countrySearch = document.getElementById('country-search');
     const regionSelected = document.getElementById('region');
     const backButton = document.getElementById('btn-back');
 
@@ -10,49 +10,66 @@ document.addEventListener('DOMContentLoaded', () => {
         var container = document.createElement('div');
         container.classList.add('container');
         container.setAttribute('country', country.name)
+        container.classList.add('drop-shadow');
+
         var countryImageContainer = document.createElement('div');
+
         var countryImage = document.createElement('img');
         countryImage.src = country.flag;
         countryImageContainer.appendChild(countryImage);
         countryImageContainer.classList.add('image-container');
         container.appendChild(countryImageContainer);
+
         var countryDescription = document.createElement('div');
         countryDescription.classList.add('country-description');
+
         var countryName = document.createElement('h2');
         countryName.innerText = country.name;
         countryDescription.appendChild(countryName);
+
         var countryPopulation = document.createElement('p');
         countryPopulation.innerText = `Population: `;
+
         var countryPopulationValue = document.createElement('span');
         countryPopulationValue.innerText = country.population.toLocaleString();
         countryPopulation.append(countryPopulationValue);
         countryDescription.appendChild(countryPopulation);
+
         var countryRegion = document.createElement('p');
         countryRegion.innerText = `Region: `;
+
         var countryRegionValue = document.createElement('span');
         countryRegionValue.innerText = country.region;
         countryRegion.append(countryRegionValue);
         countryDescription.appendChild(countryRegion);
+
         var countryCapital = document.createElement('p');
         countryCapital.innerText = `Capital: `;
+
         var countryCapitalValue = document.createElement('span');
+
         if (country.capital) {
             countryCapitalValue.innerText = country.capital;
         }
         else {
             countryCapitalValue.innerText = "None";
         }
+
         countryCapital.append(countryCapitalValue);
+
         countryDescription.appendChild(countryCapital);
+
         container.append(countryDescription);
         container.addEventListener('click', () => {
             countryDetails(country.name);
         })
+
         countriesDiv.append(container);
     }
 
     function filterRegion(region='') {
         countriesDiv.innerText = '';
+        
         if (region == '') {
             fetch('./data.json')
             .then((response) => response.json())
@@ -90,6 +107,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     backButton.addEventListener('click', () => {
         document.getElementById("country-list").style.display = "flex";
+        document.getElementById("search-filter-bar").style.display = "flex";
         document.getElementById("country-single").style.display = "none";
     })
 
@@ -98,6 +116,7 @@ document.addEventListener('DOMContentLoaded', () => {
         .then((response) => response.json()).
         then((data) => {
             var countryData = data.find(country => country.name === countryName);
+
             if (countryData) {
                 document.getElementById("country-flag").src = countryData.flag;
                 document.getElementById("country-name").innerText = countryData.name;
@@ -105,15 +124,20 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.getElementById("country-population").innerText = countryData.population.toLocaleString();
                 document.getElementById("country-region").innerText = countryData.region;
                 document.getElementById("country-sub-region").innerText = countryData.subregion;
+
                 var countryCapital = document.getElementById("country-capital");
+
                 if (countryData.capital) {
                     countryCapital.innerText = countryData.capital;
                 }
                 else {
                     countryCapital.innerText = "None";
                 }
+
                 document.getElementById("country-top-level-domain").innerText = countryData.topLevelDomain;
+                
                 var countryCurrencies = document.getElementById("country-currencies");
+                
                 if (countryData.currencies) {
                     countryCurrencies.innerText = "";
                     countryData.currencies.forEach(currency => {
@@ -121,27 +145,53 @@ document.addEventListener('DOMContentLoaded', () => {
                     })
                 }
                 else {
-                    countryCurrencies.innerText = 'None';
+                    var noneText = document.createElement('span');
+                    noneText.innerText = 'None';
+                    countryCurrencies.append(noneText);
                 }
+                
                 var countryLanguages = document.getElementById("country-languages");
                 countryLanguages.innerText = "";
+
                 countryData.languages.forEach(language => {
                     countryLanguages.innerText += `${language.name}, `;
                 })
                 countryLanguages.innerText = countryLanguages.innerText.slice(0, -2);
+                
                 var countryBorders = document.getElementById("country-borders");
+                
                 if (countryData.borders) {
-                    countryBorders.innerText = countryData.borders;
+                    countryBorders.innerText = '';
+                    countryData.borders.forEach(border => {
+                        var countryBorderButton = document.createElement('button');
+                        countryBorderButton.classList.add('drop-shadow');
+                        var borderData = data.find(country => country.alpha3Code === border);
+                        countryBorderButton.innerText = borderData.name;
+                        countryBorders.append(countryBorderButton);
+
+                        countryBorderButton.addEventListener('click', () => {
+                            countryDetails(borderData.name);
+                        })
+                    })
                 }
                 else {
-                    countryBorders.innerText = "None";
+                    countryBorders.innerHTML = '';
+                    var noneP = document.createElement('p');
+                    var noneSpan = document.createElement('span');
+                    noneSpan.innerText = 'None';
+                    noneP.append(noneSpan)
+                    countryBorders.append(noneP);
                 }
+                
                 document.getElementById("country-list").style.display = "none";
+                document.getElementById("search-filter-bar").style.display = "none";
                 document.getElementById("country-single").style.display = "flex";
             }
             else {
                 console.log(`Country ${countryName} not found`);
             }
+            
+            window.scrollTo({ top: 0 });
         })
         .catch( error => {
             console.error('The was a problem fetching the data.', error)
@@ -173,7 +223,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
       
         // update the button text
-        const newCta = newTheme === "dark" ? "Dark mode" : "Light mode";
+        const newCta = newTheme === "dark" ? "Dark Mode" : "Light Mode";
         button.innerText = newCta;  
       
         // use an aria-label if you are omitting text on the button
